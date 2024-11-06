327351aef30b352eb6289e844df25ad7d80f7bc9
KageniLinah <linahkageni3@gmail.com>
2024-11-06 16:21:15 +0300

    Updated README with se_Day2

---
+++ # Control flow example for grading stude.py
@@ -0,0 +1,7 @@
+# Defining a function
+def greet(name):
+    print("Hello, " + name + "! 👋")
+
+# Calling the function
+greet("chakin")  # Outputs: Hello, Chakin! 👋
+greet("everyone")    # Outputs: Hello, everyone! 👋
